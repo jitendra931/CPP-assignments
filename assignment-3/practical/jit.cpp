f693f26fd3ab5182cf42f66c7aba1ca5e742eba3
Jitendra Ahirwar <jitendraa7389@gmail.com>
2025-04-03 14:11:07 +0530

    Create jit.cpp

---
+++ assignment-3/practical/jit.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+class person{
+    private:
+        string name;
+        int age;
+
+    public:
+        void setvalues(string n, int a){
+            name = n;
+            age = a;
+        }
+
+        void getvalue(){
+            cout<<"\nName : "<<name<<"\nAge : "<<age;
+        }
+
+};
+
+int main(){
+    person p;
+
+    p.setvalues("Jitenra", 19);
+    p.getvalue();
+    p.setvalues("raj", 18);
+    p.getvalue();
+
+    return 0;
+}
